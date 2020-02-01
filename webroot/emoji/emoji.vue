3778e35f14f55ad5a51ef300b2cfc77a992bc051
amenoyoya <mtyswe3@gmail.com>
2020-02-02 01:23:08 +0900

    🍱 add: emoji: vue-flash-message

--- webroot/emoji/emoji.vue
+++ webroot/emoji/emoji.vue
@@ -4,6 +4,7 @@
     <div class="panel-block">
       <div class="field">
         <label class="label">{{ code }}</label>
+        <FlashMessage></FlashMessage>
         <div class="controll is-center">
           <button class="button is-button is-size-3 copy-button" :data-clipboard-text="str">
             {{ str }}
@@ -16,6 +17,15 @@
 
 <script>
 const ClipboardJS = require('clipboard/dist/clipboard.min.js')
+import Vue from 'vue'
+import FlashMessage from '@smartweb/vue-flash-message'
+
+Vue.use(FlashMessage, {
+  name: '$flashMessage',
+  tag: 'FlashMessage',
+  time: 8000,
+  icon: true
+})
 
 export default {
   props: ['title', 'str', 'code'],
@@ -26,14 +36,18 @@ export default {
     }
   },
   mounted() {
+    const vue = this;
     // クリップボードイベント準備
-    this.clipboard.on('success', e => {
-      console.log('Action:', e.action)
-      console.log('Text:', e.text)
-      console.log('Trigger:', e.trigger)
+    vue.clipboard.on('success', e => {
       e.clearSelection()
+
+      vue.$flashMessage.show({
+        status: 'success',
+        title: 'Emoji Copied',
+        message: e.text + ' is copied !'
+      })
     })
-    this.clipboard.on('error', e => {
+    vue.clipboard.on('error', e => {
       console.log('Action:', e.action)
       console.log('Trigger:', e.trigger)
     })
