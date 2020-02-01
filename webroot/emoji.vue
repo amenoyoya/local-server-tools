3b9aa36dfdca4cf2abea4e8815b59aa03a2486f1
amenoyoya <mtyswe3@gmail.com>
2020-02-01 14:36:20 +0900

    🍱 add: gitemoji by zero-server-vue

---
+++ webroot/emoji.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">gitemoji</h1>
+      <p class="subtitle">Gitで使う絵文字テンプレート</p>
+      <button class="button is-large">🍱</button>
+    </div>
+  </section>
+</template>
+
+<script>
+require('bulma')
+
+export default {
+  methods: {
+  }
+}
+</script>
