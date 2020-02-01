aa425a9d4c28386b27969b06b2e2ab0a2742dcff
amenoyoya <mtyswe3@gmail.com>
2020-02-01 23:14:59 +0900

    🎨 refactor: /emoji/

---
+++ webroot/emoji/emoji.vue
@@ -0,0 +1,34 @@
+<template>
+  <article class="panel is-success is-tile">
+    <p class="panel-heading">{{ title }}</p>
+    <div class="panel-block">
+      <div class="field">
+        <label class="label">{{ code }}</label>
+        <div class="controll is-center">
+          <button class="button is-button is-size-3">{{ str }}</button>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['title', 'str', 'code']
+}
+</script>
+
+<style lang="scss" scoped>
+article.is-tile {
+  width: 320px;
+  height: 200px;
+}
+.controll {
+  &.is-center {
+    padding: 5px 10px;
+  }
+  .button {
+    width: 280px;
+  }
+}
+</style>
---
+++ webroot/emoji/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>gitemoji</title>
+  </head>
+  <body>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">gitemoji</h1>
+        <p class="subtitle">Gitで使う絵文字テンプレート</p>
+        <div class="tile is-parent" v-masonry transition-duration="0.3s" gutter="20">
+          <div class="tile is-child" v-masonry-tile v-for="(emoji, index) in emojis" :key="'emoji_' + index">
+            <emoji :title="emoji.title" :str="emoji.str" :code="emoji.code" />
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+  </html>
+</template>
+
+<script>
+import 'bulma'
+import Vue from 'vue'
+import {VueMasonryPlugin} from 'vue-masonry'
+import emoji from './emoji'
+
+// vue-masonry 利用
+Vue.use(VueMasonryPlugin)
+
+export default {
+  data() {
+    return {
+      emojis: [
+        { title: 'initial commit', str: "🎉", code: ":party:" },
+        { title: 'add', str: "🍱", code: ":bento:" },
+        { title: 'fix', str: "🐛", code: ":bug:" },
+        { title: 'develop', str: "✨", code: ":sparkles:" },
+        { title: 'remove', str: "🔥", code: ":fire:" },
+        { title: 'refactor', str: "🎨", code: ":palette:" },
+        { title: 'hotfix', str: "🚑", code: ":ambulance:" },
+        { title: 'typo', str: "📓", code: ":notebook:" },
+        { title: 'change', str: "🔧", code: ":wrench:" },
+        { title: 'update', str: "⏫", code: ":up_pointing:" },
+        { title: 'downgrade', str: "⏬", code: ":down_pointing:" },
+        { title: 'test', str: "👽", code: ":alien:" },
+        { title: 'wip', str: "🚧", code: ":construction:" },
+        { title: 'document', str: "📚", code: ":books:" },
+        { title: 'release', str: "🔖", code: ":bookmark:" },
+      ]
+    }
+  },
+  components: {
+    'emoji': emoji
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tile {
+  &.is-child {
+    padding-bottom: 20px;
+  }
+}
+</style>
